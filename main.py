5ebba5321fec57bd35e13b13ce5437bf9af43fa3
tilontare9353-art <tilontare9353@gmail.com>
2026-01-09 19:19:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -396,4 +396,9 @@ def build_app() -> Application:
 
     return app
 
-
+if __name__ == "__main__":
+    app = build_app()
+    app.run_polling(
+        close_loop=False,
+        allowed_updates=Update.ALL_TYPES,
+    )
